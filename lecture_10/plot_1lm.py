4deae9e460473cf5f6e3541ceaf0b53271fab89e
Jonathan Sievers <sievers@cita.utoronto.ca>
2018-10-15 04:15:26 -0400

    added lecutre 10

---
+++ lecture_10/plot_1lm.py
@@ -0,0 +1,25 @@
+import numpy as np
+from matplotlib import pyplot as plt
+import healpy
+#let's make a plot of individual Ylm's
+l=10
+m=10
+
+nlm=(l+1)*(l+2)/2  #where does this come from?
+
+#healpix indexing is such that all the m=0 modes are 
+#first, m=1 modes are next, etc. use this bit to brute-force
+#find where in healpix ordering our requested mode will 
+icur=0
+for mm in range(m):
+    icur=icur+(l+1-mm)
+icur=icur+(l-m)
+
+alm=np.zeros(nlm,dtype='complex')
+alm[icur]=1.0
+nside=256
+map=healpy.alm2map(alm,nside)
+plt.ion()
+healpy.mollview(map)
+plt.title('l='+repr(l)+', m='+repr(m))
+
